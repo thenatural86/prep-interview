6304708dcc180119c051fe7c4b1352ee921b3b94
thenatural86 <philip.smalls86@gmail.com>
2024-06-12 18:00:14 -0300

    reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,15 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = ''
 
-module.exports = reverse;
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]
+  }
+  return reversed
+}
+
+reverse('asdf')
+
+module.exports = reverse
