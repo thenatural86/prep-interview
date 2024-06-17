253fbbd2b250e1499e7832c5cc6077af7a2b1e04
thenatural86 <philip.smalls86@gmail.com>
2024-06-17 11:48:45 -0300

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// helper function
+function buildCharMap(str) {
+  const charMap = {}
+  // Convert the string to lower case and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^\w]/g, '')
 
-module.exports = anagrams;
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
+
+function anagrams(stringA, stringB) {
+  const charMapA = buildCharMap(stringA)
+  const charMapB = buildCharMap(stringB)
+
+  // Compare the length of the character maps
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+module.exports = anagrams
--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -14,7 +14,7 @@
 
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log('fizzbuzz')
     } else if (i % 3 === 0) {
       console.log('fizz')
