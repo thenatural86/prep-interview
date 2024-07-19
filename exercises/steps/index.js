6918d5e9c8af64eafa9cdd0255da56b6d6fed5a7
thenatural86 <philip.smalls86@gmail.com>
2024-07-19 11:14:34 -0300

    reset

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,6 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let steps = ''
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        steps += '#'
-      } else {
-        steps += ' '
-      }
-    }
-    console.log(steps)
-  }
-}
+function steps(n) {}
 
 module.exports = steps
